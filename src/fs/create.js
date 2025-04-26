51c925c5a38c869efb176dec96c6e8312135c139
imagineapril <julia.timokhina604@gmail.com>
2025-04-26 23:15:18 +0300

    feat: add create task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const create = async () => {
-    // Some code here 
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+    const content = 'I am fresh and young';
+
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+    } catch (err) {
+        if (err.code !== 'ENOENT') throw err;
+    }
+
+    const dirPath = path.join(__dirname, 'files');
+    await fs.mkdir(dirPath, { recursive: true });
+
+    await fs.writeFile(filePath, content);
 };
 
 await create();
